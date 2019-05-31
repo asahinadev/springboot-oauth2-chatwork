73ad713c21ac2aa1d1c8f7824efeec274c046052
asahinadev <dev.account.01@mail.mirageworld.jp>
2019-06-01 08:40:08 +0900

    maven -> gradle

--- src/main/java/com/example/spring/SpringOauth2ChatworkApplication.java
+++ src/main/java/com/example/spring/SpringOauth2ChatworkApplication.java
@@ -1,14 +1,13 @@
-package com.example.spring.chatwork;
+package com.example.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class SpringOauth2ChatworkApplication {
 
 	public static void main(String[] args) {
-
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(SpringOauth2ChatworkApplication.class, args);
 	}
 
 }
